dbf1c910234b95f379d830fd1fbaee542086dd13
Aécio Santos <aecio.solando@gmail.com>
2025-01-15 22:28:27 -0500

    feat: Added functions match_values and materialize_mapping

--- src/bdikit_context/agent.py
+++ src/bdikit_context/agent.py
@@ -17,8 +17,8 @@ class BDIKitAgent(BeakerAgent):
 
         Args:
             dataset (str): The name of the dataset variable.
-            target (str): The target table or standard data vocabulary. Defaults to “gdc”.
-            method (str): The method used for mapping. Defaults to “coma”.
+            target (str): The target table or standard data vocabulary. Defaults to "gdc".
+            method (str): The method used for mapping. Defaults to "ct_learning".
 
         Returns:
             str: returns the matched columns
@@ -42,6 +42,7 @@ class BDIKitAgent(BeakerAgent):
         match_result = result.get("return")
 
         return match_result
+
 
     @tool()
     async def top_matches(self, dataset: str, columns: str, target: str, agent: AgentRef) -> str:
@@ -75,4 +76,127 @@ class BDIKitAgent(BeakerAgent):
 
         match_result = result.get("return")
 
-        return match_result        
+        return match_result
+
+
+    @tool()
+    async def match_values(self, dataset: str, column_mapping: str, target: str, method: str, agent: AgentRef) -> str:
+        """
+        Returns the top 10 value matches between the value of the source and target columns.
+        This is useful for evaluating value matches between a pair columns (column mappings) returned by the match_schema function.
+
+        Args:
+            dataset (str): The name of the dataset variable.
+            column_mapping (tuple): The column and target names for which to find value matches for. The values must be separated by a comma, example: "source_column,target_column"
+            target (str): The target table or standard data vocabulary. Defaults to “gdc”.
+            method (str): The method used for mapping. Defaults to “tfidf”.
+
+        Returns:
+            str: returns the value matches for the given column mapping (source and target column names)
+
+        You should show the user the result after this function runs.
+        Uppon user's request, the output of match_values() can be fed to materialize_mapping() which materializes the final target using both schema and value mappings.
+        """
+
+        code = agent.context.get_code(
+            "match_values",
+            {
+                "dataset": dataset,
+                "column_mapping": tuple(column_mapping.split(',')),
+                "target": target,
+                "method": method,
+            },
+        )
+        result = await agent.context.evaluate(
+            code,
+            parent_header={},
+        )
+
+        match_result = result.get("return")
+
+        return match_result
+
+
+    @tool()
+    async def materialize_mapping(self, dataset: str, mapping_spec: str, agent: AgentRef) -> str:
+        """
+        Materializes the final (harmonized) table after applying data transformations specified by the schema mapping and value mappings specifications.
+
+        This function takes as input the source dataset and the mapping specifications
+        (schema and value mappings) formated as a MappingSpecLike dictionary object.
+        The `MappingSpecLike` is a type alias that specifies mappings between source
+        and target columns. It must include the source and target column names
+        and a value mapper object that transforms the values of the source column
+        into the target.
+
+        The mapping specification can be (1) a DataFrame or (2) a list of dictionaries or DataFrames.
+        If it is a list of dictionaries, they must have:
+        - `source`: The name of the source column.
+        - `target`: The name of the target column.
+        - `mapper` (optional): A ValueMapper instance or an object that can be used to
+          create one using :py:func:`~bdikit.api.create_mapper()`. Examples of valid objects
+          are Python functions or lambda functions. If empty, an IdentityValueMapper
+          is used by default.
+        - `matches` (optional): Specifies the value mappings. It must be a list of tuples
+          containing a pair of source and target values (<source_value>, <target_value>).
+          Please make sure to always represent pairs of source and target values using
+          Python tuples (with parenthesis). Do NOT use a lists of lists.
+
+        Alternatively, the list can contain DataFrames. In this case, the DataFrames must
+        contain not only the value mappings (as described in the `matches` key above) but
+        also the `source` and `target` columns as DataFrame attributes. The DataFrames created
+        by :py:func:`~bdikit.api.match_values()` include this information by default.
+        If the mapping specification is a DataFrame, it must be compatible with the dictionaries
+        above and contain `source`, `target`, and `mapper` or `matcher` columns.
+
+        Example:
+
+        .. code-block:: python
+
+            mapping_spec = [
+                {
+                    # When no value mapping is need, specifying the source and target is enough
+                    "source": "source_column1",
+                    "target": "target_column1",
+                },
+                {
+                    # Data transformations can be specified using a mapper, which can be a custom Python lambda function (or a regular function)
+                    "source": "source_column2",
+                    "target": "target_column2",
+                    "mapper": lambda age: -age * 365.25,
+                },
+                {
+                    "source": "source_column3",
+                    "target": "target_column3",
+                    "matches": [
+                        ("source_value1", "target_value1"),
+                        ("source_value2", "target_value2"),
+                    ]
+                }
+            ]
+
+        Args:
+            dataset (str): The name of the dataset variable.
+            mapping_spec (MappingSpecLike): the column and value mapping specificiation.
+
+        Returns:
+            str: returns the materialized target using both schema and value mappings
+
+        After this function runs, you should show a sample of 10 rows of the materialized dataframe to the user.
+        """
+
+        code = agent.context.get_code(
+            "materialize_mapping",
+            {
+                "dataset": dataset,
+                "mapping_spec": mapping_spec,
+            },
+        )
+        result = await agent.context.evaluate(
+            code,
+            parent_header={},
+        )
+
+        materialize_result = result.get("return")
+
+        return materialize_result
--- src/bdikit_context/context.py
+++ src/bdikit_context/context.py
@@ -17,14 +17,29 @@ class BDIKitContext(BeakerContext):
 
     async def auto_context(self):
             return f"""
-            You are an assistant helping biomedical researchers harmonize their data. If you are asked to perform a Pandas operation
-            you should NOT print the results by default. Instead, acknowledge that the operation has been performed. The user can print
-            the head of the dataframe if they so choose.
+            You are an assistant helping biomedical researchers harmonize their data.
 
-            You are an expert in the use of NYU's BDIKit library. You have access to the following functions:
-                - match_schema: This function performs schema mapping between the source table and the given target schema. 
+            You are an expert in the use of BDIKit library. You have access to the following functions:
+                - match_schema: This function performs schema mapping between the source table and the given target schema.
                 - top_matches: Returns the top 10 schema matches between the source and target tables. This is useful for evaluating alternative column mappings.
-            
-            Once you have run `match_schema` you should take a look at the matches, if any seem incorrect you can run `top_matches` to see alternative matches.
-            It is a good idea to show the user the result after each function runs.
+                - match_values: Finds matches between column values from the source dataset and column values of the target schema.
+                - materialize_mapping: Materializes the final (harmonized) table after applying data transformations specified by the schema mapping and value mappings specifications.
+
+            Data harmonization consists of the following steps:
+             1. For all columns in the source table, find the best column mappings in the target GDC schema using match_schema and top_matches.
+             2. For each discovered pair of column mappings, find the best value mappings in the source and target columns using match_values.
+             3. Materializing a harmonized table after applying the schema and value mappings discovered in steps 1 and 2 using materialize_mapping.
+
+            In step 1, once you have run `match_schema` you should take a look at the matches, if any pair of columns seems
+            incorrect (e.g., if they are semantically different) you can run `top_matches` to see alternative column matches.
+            The best alternative column match is not always the one with the highest score, you should consider the
+            meaning of the column name to select the best match. If unsure, you can show the plausible alternatives to the user
+            and ask for a user confirmation.
+
+            In step 2, once you find the best value mappings using `match_values`, you should compare each pair of matches
+            to check if they are correct, and then show alternative corrections. Always show the errors to the user and suggested corrections.
+
+            You should always show the user a list of matches (source and target) after each function runs. 
+            For example, after running `match_schema` you should print a list of all column matches.
+            If a match has been corrected, you should show the suggestion in parenthesis.
             """.strip()
